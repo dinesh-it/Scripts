c8e1764d59660d14697957b22ea6418d8661be9c
Dinesh D <dd.dinesh.rajakumar@gmail.com>
2019-02-21 17:51:15 +0530

    Updated new machine setup requirements

--- new_setup.sh
+++ new_setup.sh
@@ -1,24 +1,24 @@
 sudo apt-get update
 
 # essential tools for work
-sudo apt-get install openssh-client openssh-server cpanminus ctags git postgresql-9.5 vim byobu sox libpq-dev
+sudo apt-get install openssh-client openssh-server cpanminus ctags git vim byobu sox libpq-dev openjdk-9-jre python-setuptools python-dev nmap tcl build-essential xclip ack apache2 apache2-utils python-setuptools python-dev build-essential redis-server nodejs perl-doc autorun autoconf libtool resolvconf openresolv network-manager-openvpn network-manager-openvpn-gnome pavucontrol npm
 
-# essential tools - personal
-sudo apt-get install namp wine tcl build-essential xclip ack-grep
+# Install Postgress
+sudo apt install postgresql-9.6
 
 # For kde connect and indicator
-sudo add-apt-repository ppa:vikoadi/ppa
+# XXX: Not available from 17.10
+# sudo add-apt-repository ppa:vikoadi/ppa
+# sudo apt-get update
 
-# For cinnamon desktop
-sudo add-apt-repository ppa:embrosyn/cinnamon
-sudo apt-get update
+# To install extra apps etc..
+sudo apt-get install kdeconnect perlbrew disper
 
-# To install extra apps, cinnamon, themes, etc..
-sudo apt-get install indicator-kdeconnect kdeconnect perlbrew cinnamon blueberry numix-gtk-theme disper
+sudo cpanm Data::Dumper Term::Size::Any Catalyst::Plugin::Cache Catalyst::Authentication::Store::DBIx::Class Log::Log4perl JSON::WebToken Config::JSON Sysadm::Install File::Type DBD::Pg Perl::Tidy Data::UUID::MT DBIx::Class::Schema::Loader Module::Build
 
-# clean up
-sudo apt-get autoremove
+if [ $1 ]
+do
+sudo cpanm <$1 
+done
 
-sudo cpanm Data::Dumper Term::Size::Any Catalyst::Plugin::Cache Catalyst::Authentication::Store::DBIx::Class Log::Log4perl JSON::WebToken Config::JSON Sysadm::Install File::Type DBD::Pg 
-
-sudo cpanm <doc/modlist 
+echo "Install sox, dropbox, skype, slack, telegram by direct download deb's"
